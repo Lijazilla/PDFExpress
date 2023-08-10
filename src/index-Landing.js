755c38bc40fd5445d775248f4605609ecee990ea
alex1986 <alejandro-1986-jf@hotmail.com>
2023-08-10 09:31:06 -0600

    respaldo de landingpage 10/08/2023

--- src/index-Landing.js
+++ src/index-Landing.js
@@ -4,41 +4,38 @@ import './index-Landing.css';
 
 const LandingPage = () => {
     return (
-        <div>
-            <section className="container-index-navbar">
+        <div className="body-container">
+            <header className="container-index-navbar">
                 <div className="logo-div">
-                    {/* <img src="./images/PDFpro_logo.png" alt="PDFpro_logo" class="logo"> */}
+
                 </div>
 
-                <div className="index-navbar-center">
-                    <nav className="main-navbar-list">
+                <navbar className="index-navbar-center">
                         <ul className="ul-main-navbar-list">
                             <li>Home</li>
-                            <li>About us</li>
                             <li>Pricing</li>
                             <li>Contact</li>
                         </ul>   
-                    </nav>
-                </div>
+                </navbar>
 
                 <div className="index-navbar-right">
                     <button>Inicia sesión</button>
                     <button>Registrate</button>
                 </div>
-            </section>
+            </header>
 
             <section className="body-main-container">
                 <div className="index-main-title-container">
                     <div className="index-h1-title-container">
-                        <h1>Genera reportes de tus Servicios en minutos.</h1>
-                        <h2>Mandalo a tus clientes al momento.</h2>
+                        <h1 className="main-container-h1">PDF Express</h1>
+                        <h2 className="main-container-h2">Genera reportes de tus Servicios </h2>
+                        <h3 className="main-container-h3">Mandalo a tus clientes al momento.</h3>
                     </div> 
                     <div className="index-text-main-container">
-                        <ul>
-                            <li>Paso 1 - Inicia sesión en nuestra plataforma.</li>
-                            <li>Paso 2 - Escribe el texto de reporte.</li>
-                            <li>Paso 3 - Adjunta o toma las fotos al momento.</li>
-                            <li>Paso 4 - Crea el documento y envíalo a tus clientes.</li>
+                        <ul className="text-main-container-list">
+                            <li>Paso 1 - Escribe el texto de reporte.</li>
+                            <li>Paso 2 - Adjunta o toma las fotos al momento.</li>
+                            <li>Paso 3 - Envíalo a tus clientes.</li>
                         </ul>
                     </div> 
                 </div>
@@ -48,7 +45,35 @@ const LandingPage = () => {
                 
             </section>
 
-            <section>
+            <section className="text-between-sections-container">
+                <h1 className="text-between-sections-h1">Simplifica tu proceso de reportes:</h1>
+                <h2 className="text-between-sections-h2">Convierte la generación y 
+                    envío de informes en un proceso rápido y sencillo, 
+                    directamente desde tu dispositivo móvil o escritorio.</h2>
+            </section>
+
+            <section className="second-container-benefits">
+                
+                <div className="second-body-main-container">
+                    <div className="second-index-main-title-container">
+                        <div className="second-index-h1-title-container">
+                            <h1 className="second-main-container-h1">PDF Express</h1>
+                            <p className="second-main-container-p">Reportes profesionales en minutos: Olvídate de horas de trabajo en la oficina. 
+                            Crea informes detallados y visualmente atractivos al instante, estés donde estés.</p>
+                        </div> 
+                        <img ></img>
+
+                        <div className="second-index-text-main-container">
+
+                        </div> 
+                    </div>
+                    <div className="second-body-img-main-container">
+                    <p className="second-main-container-p2">Muestra tu trabajo con confianza: Proporciona a tus clientes evidencia visual
+                                                                    del servicio realizado en tiempo real, fortaleciendo la confianza en tu trabajo.</p>
+                        
+                    </div>
+                    
+                </div>
                 
             </section>
             {/* <Link to="/app">Ir a la App</Link> */}
